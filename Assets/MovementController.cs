7ab66c9269796cc8b3e3683cdf05600642538197
s19208 <s19208@pjwstk.edu.pl>
2020-10-31 14:04:54 +0100

    simple player movement

---
+++ Assets/MovementController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rigidbody2D;
+    [SerializeField] private float speed = 5;
+    
+    private Vector3 movement;
+    void Start()
+    {
+        rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = 0;
+
+    }
+
+    void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(transform.position + movement * (speed * Time.deltaTime));
+    }
+
+    private void HandleInput()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        movement = new Vector2(horizontalMovement, verticalMovement).normalized;
+    }
+}
