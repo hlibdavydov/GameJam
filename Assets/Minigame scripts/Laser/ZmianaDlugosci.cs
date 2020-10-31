fa26527ab03dce7126b770c12d40bbb5797a7afd
s18890 <s18890@pjwstk.edu.pl>
2020-10-31 18:40:19 +0100

    Minigra lasery

---
+++ Assets/Minigame scripts/Laser/ZmianaDlugosci.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZmianaDlugosci : MonoBehaviour
+{
+
+    float changeSpeed = 0.005f;
+    float timeToChange = 3f;
+    bool shorten;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shorten = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToChange -= Time.deltaTime;
+        if (timeToChange <= 0)
+        {
+            timeToChange = 1.8f;
+            if (shorten)
+            {
+                shorten = false;
+            }
+            else
+            {
+                shorten = true;
+            }
+        }
+        if (shorten)
+        {
+            transform.localScale -= new Vector3(0, changeSpeed, 0);
+        }
+        else
+        {
+            transform.localScale += new Vector3(0, changeSpeed, 0);
+        }
+    }
+}
