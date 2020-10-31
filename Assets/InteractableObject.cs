1524bbe22674a28af9c09690e079f9d89dc50b70
s19208 <s19208@pjwstk.edu.pl>
2020-10-31 18:01:43 +0100

    interactable object added

---
+++ Assets/InteractableObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    [SerializeField] private GameObject tooltip;
+    private bool isActive;
+
+    private void Update()
+    {
+        if()
+    }
+
+    private void Start()
+    {
+        tooltip.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ShowTooltip();
+        isActive = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        tooltip.SetActive(false);
+        isActive = false;
+    }
+
+    private void ShowTooltip()
+    {
+        tooltip.SetActive(true);
+    }
+}
