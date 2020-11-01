f73f9d543b3fa53aa24e0afb39d93002b9341ed5
Oleksii Arskyi <s18898@pjwstk.edu.pl>
2020-11-01 06:53:17 +0200

    Guardians commit

---
+++ Assets/Scripts/GuardMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMovement:MonoBehaviour
+{
+    public float pause=1f;
+    public float speed=3f;
+    public float triggeredSpeed=2f;
+    [HideInInspector]
+    public bool isTriggered=false;
+    private Rigidbody2D guardBody;
+    private Vector3 movement;
+    private float timeLeft;
+    public void SetTriggerValue(bool value)
+    {
+        this.isTriggered=value;
+    }
+    public void Start()
+    {
+        this.guardBody=GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        this.timeLeft-=Time.deltaTime;
+        if(this.timeLeft<=0)
+        {
+            this.movement=new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+            this.timeLeft+=this.pause;
+        }
+        if(this.isTriggered)
+        {
+            this.movement=GameObject.Find("Player").transform.position-transform.position;
+            transform.Translate(this.movement*this.triggeredSpeed*Time.deltaTime);
+        }
+        else this.guardBody.MovePosition(transform.position+this.movement*this.speed*Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement:MonoBehaviour
+{
+    public float speed=30f;
+    private Rigidbody2D playerBody;
+    private Vector3 movement;
+    public void Start()
+    {
+        this.playerBody=GetComponent<Rigidbody2D>();
+        this.playerBody.gravityScale=0;
+    }
+    public void Update()
+    {
+        this.movement=new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+    }
+    public void FixedUpdate()
+    {
+        this.playerBody.MovePosition(transform.position+this.movement*this.speed*Time.deltaTime);
+    }
+}
