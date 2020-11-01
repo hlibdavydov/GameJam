d53e176c7e93f1b62009913e6df6419dfe5a111c
s18890 <s18890@pjwstk.edu.pl>
2020-11-01 01:26:31 +0100

    Scream moving
    Scream has unassigned animation of moving

---
+++ Assets/ScreamAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ScreamAnimation : MonoBehaviour
+{
+
+    [SerializeField] bool startAnimation;
+    [SerializeField] float distanceToTravel = 0.5f;
+    [SerializeField] float timeToTravel = 2f;
+    float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startAnimation = false;
+        speed = distanceToTravel / timeToTravel;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startAnimation)
+        {
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+            distanceToTravel -= speed * Time.deltaTime;
+
+            if (distanceToTravel <= 0)
+            {
+                startAnimation = false;
+            }
+        }
+    }
+}
