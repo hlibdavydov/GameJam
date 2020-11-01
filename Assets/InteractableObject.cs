e091edfb9f1af28c82c9f7fc9e8c9b9cc59b9f58
s19208 <s19208@pjwstk.edu.pl>
2020-11-01 05:32:23 +0100

    level connection,level extension, scrolls, were added

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -6,38 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class InteractableObject : MonoBehaviour
 {
-    [SerializeField] private GameObject tooltip;
+    [SerializeField] public GameObject tooltip;
     [SerializeField] private string scene;
-    
+    public Action OnInteraction;
     private bool isActive;
 
+    
+    private void Start()
+    {
+        OnInteraction = () =>
+        {
+            if (scene == null) return;
+            SceneManager.LoadScene(scene);
+        };
+        tooltip.SetActive(false);
+    }
     private void Update()
     {
         if (!isActive) return;
         if (Input.GetKeyDown(KeyCode.E))
         {
-            LoadMinigame();
+            OnInteraction();
         }
     }
 
-    private void LoadMinigame()
-    {
-        SceneManager.LoadScene(scene);
-    }
-
-    private void Start()
-    {
-        tooltip.SetActive(false);
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (tooltip == null) return;
+        if (other.GetComponent<Player>() == null) return;
         ShowTooltip();
         isActive = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (tooltip == null) return;
+        if (other.GetComponent<Player>() == null) return;
         tooltip.SetActive(false);
         isActive = false;
     }
---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerPosition
+    {
+        public Vector3 position;
+    }
+}
